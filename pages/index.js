1a1b6304e3657dfd3ba8a785a13944dce4bb70ae
teebow <thibaud@weinachter.com>
2019-05-15 08:05:43 +0000

    nex-tuto: deploy with now

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,22 @@ const PostLink = ({ show }) => (
     <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
       <a>{show.name}</a>
     </Link>
+     <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 )
 
