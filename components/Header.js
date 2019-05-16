351112f981bbd42de7634879ea54e14e7de0a569
teebow <thibaud@weinachter.com>
2019-05-16 15:04:30 +0000

    start login screen

--- components/Header.js
+++ components/Header.js
@@ -13,6 +13,9 @@ const Header = () => (
         <Link href="/users">
             <a style={linkStyle}>Users</a>
         </Link>
+        <Link href="/login">
+            <a style={linkStyle}>Login</a>
+        </Link>
     </div>
 )
 
