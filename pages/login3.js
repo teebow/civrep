01ee454eb016dbb590ee8193f6db87fe4855db9c
tibo <tibo@macbook-pro-de-tibo-1.home>
2019-05-26 20:35:51 +0200

    start using styled component

--- pages/login3.js
+++ pages/login3.js
@@ -1,28 +1,47 @@
 import React from "react";
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import Link from 'next/link'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-
 
 import LoginCard from '../components/Login/LoginCard';
 
 import Logo from '../static/logo.svg';
 import theme from "../styles/theme";
 
-const Login = (props) => (
+import styled from 'styled-components';
 
-    <div className="root">
+const Root = styled.div`
+    height: 100vh;
+    min-height:100%;
+`;
+
+const Container = styled.div`
+    background-image: linear-gradient(to right top, #062c3f, #064664, #03628c, #0080b6, #009ee2);
+    background-repeat: no-repeat;
+    padding-top: 32px;
+    align-items: center;
+    min-height:100%;
+    background-size: 100% 300px;
+`
+
+const LogoContainer = styled.div`
+    width: 120px;
+    margin: 0 auto;
+`
+
+const Login = (props) => (
+    <Root>
         <MuiThemeProvider theme={theme}>
             <CssBaseline />
-            <div className="container">
+            <Container>
                 <Grid container={true} justify="center" alignItems="center" spacing={3} >
                     <Grid item xs={8}>
-                        <div className="logo">
+                        <LogoContainer>
                             <Logo />
-                        </div>
+                        </LogoContainer>
                     </Grid >
                     <Grid item xs={8}>
                         <LoginCard theme={theme} />
@@ -33,29 +52,9 @@ const Login = (props) => (
                         </Link>
                     </Grid>
                 </Grid>
-            </div>
+            </Container>
         </MuiThemeProvider>
-        <style jsx>{`
-        .logo {
-            width: 120px;
-            margin: 0 auto;
-        }
-        .container {
-            background-image: linear-gradient(to right top, #062c3f, #064664, #03628c, #0080b6, #009ee2);
-            background-repeat: no-repeat;
-            padding-top: 32px;
-            align-items: center;
-            min-height:100%;
-            background-size: 100% 300px;
-        }
-        .root { 
-            height: 100vh;
-            min-height:100%;
-        };
-      `}
-        </style>
-    </div>
-
+    </Root>
 );
 
 
---
+++ style.md
@@ -0,0 +1,2 @@
+background-image: linear-gradient(to left top, #eebce8, #d8c6fb, #bbd0ff, #9fdaff, #8ce2ff, #80dff9, #75dbf3, #69d8ed, #56caeb, #48bce8, #41ade3, #439edd);
+
